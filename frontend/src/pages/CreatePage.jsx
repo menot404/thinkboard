2328ea0dd62ee40d6ff612aebb221ae9b4c66c9e
Me_Not404 <m3not404@gmail.com>
2026-04-02 16:38:24 +0000

    feat: refactor CreatePage and HomePage to use modular axios instance

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -2,7 +2,7 @@ import { Link, useNavigate } from "react-router";
 import { useState } from "react";
 import { ArrowLeftIcon } from "lucide-react";
 import toast from "react-hot-toast";
-import axios from "axios";
+import api from "../lib/axios";
 
 const CreatePage = () => {
   const [title, setTitle] = useState("");
@@ -20,7 +20,7 @@ const CreatePage = () => {
     }
     setLoading(true);
     try {
-      await axios.post("http://localhost:5001/api/notes", {title, content});
+      await api.post("/notes", {title, content});
       toast.success("Note created successfully!");
       setTitle("");
       setContent("");
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react'
-import axios from 'axios';
 import toast from 'react-hot-toast';
 
 import RateLimitedUI from '../components/RateLimitedUI';
 import NoteCard from '../components/NoteCard';
+import api from '../lib/axios';
 
 const HomePage = () => {
   const [notes, setNotes] = useState([]);
@@ -13,7 +13,7 @@ const HomePage = () => {
   useEffect(()=>{
     const fetchNotes = async()=>{
       try {
-        const res = await axios.get("http://localhost:5001/api/notes");
+        const res = await api.get("/notes");
         setNotes(res.data.notes);
         console.log(res.data.notes);
         setIsRateLimited(false);
