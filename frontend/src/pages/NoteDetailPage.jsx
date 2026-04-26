a2a9c242c132b7c10d79105367178fa059372e1f
Me_Not404 <m3not404@gmail.com>
2026-04-26 11:56:09 +0000

    ui: improve form layout on create and detail pages

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -88,27 +88,27 @@ const NoteDetailPage = () => {
 
             <div className="card bg-base-100">
               <div className="card-body">
-                <div className="form-control mb-4">
+                <div className="form-control mb-6 flex flex-col gap-2">
                   <label htmlFor="" className="label">
                     <span className="label-text">Note Title</span>
                   </label>
                   <input
                     type="text"
                     placeholder="Note Title"
-                    className="input input-border"
+                    className="input input-primary input-bordered focus:outline-0 focus:ring-2 focus:ring-primary focus:border-transparent"
                     value={note ? note.title : ''}
                     onChange={(e) => setNote({ ...note, title: e.target.value })}
                     disabled={!note}
                   />
                 </div>
 
-                <div className="form-control mb-4">
+                <div className="form-control mb-6 flex flex-col gap-2">
                   <label htmlFor="" className="label">
                     <span className="label-text">Content</span>
                   </label>
                   <textarea
                     placeholder="Write your note content here..."
-                    className="textarea textarea-bordered h-32"
+                    className="textarea textarea-bordered h-32 input-primary pl-4 pt-4 focus:outline-0 focus:ring-2 focus:ring-primary focus:border-transparent"
                     value={note ? note.content : ''}
                     onChange={(e) => setNote({ ...note, content: e.target.value })}
                     disabled={!note}
